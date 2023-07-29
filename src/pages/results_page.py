1634468939cb331306da2da7b857054b3e1e3103
Sofia Di Nonno <sofidinonno@gmail.com>
2023-07-29 17:21:35 -0300

    Project structure change

--- src/pages/results_page.py
+++
@@ -1,9 +0,0 @@
-from seleniumpagefactory.Pagefactory import PageFactory
-from base_page import BasePage
-
-class ResultPage(PageFactory, BasePage):
-    def __init__(self, driver):
-        super().__init__()
-        self.driver = driver
-
-    locators = {}
